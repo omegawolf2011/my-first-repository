56ccc1a742d7d1cb10210adc2c6c8b4110734966
omegawolf2011 <solyanaasefa20@gmail.com>
2024-06-21 17:02:04 -0500

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -2,7 +2,11 @@ function myFunction() {
     document.getElementById("demo").innerHTML="The departed is the second best movie; fun fact Leonardo Dicaprio starred in both movies.";
 }
 function changeImage() {
-    var image = document.getElementById('myImage');
-    // Replace 'new-image.jpg' with the path to your new image
-    image.src = 'https://prodimage.images-bn.com/pimages/0085391117292_p0_v2_s1200x630.jpg';
-        image.alt = 'New Image Alt Text'; // Optional: Change alt text}
+  var image = document.getElementById('myImage');
+  if (image.src.match('original.jpg')) {
+    image.src = 'new.jpg'; // Change to the new image path
+  } else {
+    image.src = 'original.jpg'; // Change back to the original image path
+  }
+}
+
