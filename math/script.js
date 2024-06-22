5c7b95d0fc13c913cb5580c6c491664bd4cd2f49
omegawolf2011 <solyanaasefa20@gmail.com>
2024-06-22 14:36:28 -0500

    Update script.js

--- math/script.js
+++ math/script.js
@@ -49,17 +49,17 @@ function calculateResult() {
 }
 
 function sum(a, b) {
-   
+   return a+b
 }
 
 function subtract(a, b) {
-   
+   return a-b
 }
 
 function multiply(a, b) {
-   
+   return a*b
 }
 
 function divide(a, b) {
-   
+   return a/b
 }
